2490d4d1ce3ccf0729a27e101ed89ff071cdc0fc
63isOK <1876180681@qq.com>
2019-10-16 08:12:38 +0000

    修改链表指针方向 - 重构

--- go/1.1.1/reverse_test.go
+++ go/1.1.1/reverse_test.go
@@ -6,23 +6,54 @@ package main
 import (
 	"container/list"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
-func TestReverse(t *testing.T) {
-	l := list.New()         // 999...1
-	want := []interface{}{} // 1...999
+func createList(lenght int, isIntValue bool) (*list.List, []interface{}) {
+	l := list.New()
+	want := []interface{}{}
 
-	for i := 1; i < 1010; i++ {
-		l.PushFront(i)
-		want = append(want, i)
+	for i := 1; i < lenght; i++ {
+		if isIntValue {
+			l.PushFront(i)
+			want = append(want, i)
+		} else {
+			l.PushFront(strconv.Itoa(i))
+			want = append(want, strconv.Itoa(i))
+		}
 	}
 
-	t.Run("change list", func(t *testing.T) {
-		got := change(l)
+	return l, want
+}
 
-		if !reflect.DeepEqual(want, got) {
-			t.Fatalf("want:%v, got:%v", want, got)
-		}
-	})
+func TestReverse(t *testing.T) {
+
+	type cases struct {
+		name string
+		l    *list.List
+		want []interface{}
+	}
+
+	intList10, intWant10 := createList(10, true)
+	intList10k, intWant10k := createList(10000, true)
+	stringList10, stringWant10 := createList(10, false)
+	stringList10k, stringWant10k := createList(10000, false)
+
+	datas := []cases{
+		{"int-10", intList10, intWant10},
+		{"int-10k", intList10k, intWant10k},
+		{"string-10", stringList10, stringWant10},
+		{"stirng-10k", stringList10k, stringWant10k},
+	}
+
+	for _, x := range datas {
+		t.Run(x.name, func(t *testing.T) {
+			got := change(x.l)
+
+			if !reflect.DeepEqual(x.want, got) {
+				t.Fatalf("want:%v, got:%v", x.want, got)
+			}
+		})
+	}
 }
