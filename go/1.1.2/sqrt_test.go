d2705e870a98b400e6731b9df84ead184fb95ec7
63isOK <1876180681@qq.com>
2019-10-18 01:22:06 +0000

    添加基准测试

--- go/1.1.2/sqrt_test.go
+++ go/1.1.2/sqrt_test.go
@@ -6,10 +6,24 @@ import (
 )
 
 func TestSqrt(t *testing.T) {
-	want := math.Sqrt2
-	got := sqrt(2, 10)
+	check(t, math.Sqrt2, sqrt(2, 10), 1e-10)
+	check(t, 0, sqrt(0, 10), 1e-10)
+}
 
-	if math.Abs(want-got) >= 1e-10 {
+func check(t *testing.T, want, got, precision float64) {
+	if math.Abs(want-got) >= precision {
 		t.Errorf("want:%.10f, got:%.10f", want, got)
+	}
+}
+
+func BenchmarkSqrt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sqrt(90, 10)
+	}
+}
+
+func BenchmarkStdSqrt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		math.Sqrt(90)
 	}
 }
