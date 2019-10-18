7a590180e2278d89bd3804542b433dab93ebaae8
63isOK <1876180681@qq.com>
2019-10-18 01:06:38 +0000

    二分法求平方根 - 绿灯

--- go/1.1.2/sqrt.go
+++ go/1.1.2/sqrt.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func sqrt(f float64, precision int) float64 {
-	return 1.4142135623
+func absDiff(a, b float64) float64 {
+	if a < b {
+		return b - a
+	}
+
+	return a - b
+}
+
+func sqrt(x float64, precision int) float64 {
+	if x < 0 {
+		return 0
+	}
+
+	min, max, cur := 0.0, float64(x), float64(x)/2
+	calcPower := cur * cur
+	lastPower := -1.0
+	pre, _ := strconv.ParseFloat(fmt.Sprintf("1e-%d", precision), 64)
+
+	for absDiff(calcPower, lastPower) >= pre {
+		if calcPower > x {
+			max = cur
+		} else {
+			min = cur
+		}
+		cur = (min + max) / 2
+		lastPower = calcPower
+		calcPower = cur * cur
+	}
+
+	return cur
 }
 
 func main() {
