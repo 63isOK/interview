8e81c6f9faaf01670bbe9c103d4de09b519374ea
63isOK <1876180681@qq.com>
2019-10-22 08:41:09 +0000

    LRU - 绿灯

--- go/1.1.4/lru.go
+++ go/1.1.4/lru.go
@@ -9,11 +9,42 @@ type element struct {
 
 type cache [4]element
 
+var index = 0
+
 func (c *cache) get(k int) int {
+	for _, x := range c {
+		if x.Key == k {
+			return x.Value
+		}
+	}
+
 	return -1
 }
 
 func (c *cache) put(k, v int) {
+	index++
+	match, older := -1, 0
+
+	for i, x := range c {
+		if x.Key == k {
+			match = i
+			break
+		}
+
+		if c[older].Index > x.Index {
+			older = i
+		}
+	}
+
+	if match != -1 {
+		c[match].Key = k
+		c[match].Value = v
+		c[match].Index = index
+	} else {
+		c[older].Key = k
+		c[older].Value = v
+		c[older].Index = index
+	}
 }
 
 func create() *cache {
